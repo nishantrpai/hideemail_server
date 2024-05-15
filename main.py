ec7331054e37734f4e43d3226ccac0590d9b15ef
Nishant Pai <nishantpai@Nishants-MacBook-Pro.local>
2024-05-15 23:10:40 +0530

    update main py

--- main.py
+++ main.py
@@ -59,8 +59,8 @@ def get_updates():
     Returns:
         list: A list of updates (messages) received by the bot.
     """
-    print("Getting updates...", GET_UPDATES_URL)
-    response = requests.get(GET_UPDATES_URL)
+    print("Getting updates...", f'https://api.telegram.org/bot{TG_TOKEN}/getUpdates')
+    response = requests.get(f'https://api.telegram.org/bot{TG_TOKEN}/getUpdates')
     data = response.json()
     print(data)
     return data['result']
